1a4540db31e3452c4e6af3497fc481cb6b9506d9
mrfrost91 <metallfrost@gmail.com>
2019-06-06 15:39:02 +0300

    Update timeonverion.js
    123

--- timeonverion.js
+++ timeonverion.js
@@ -1,8 +1,8 @@
 function timeConversion(s) {
     let hours = s.substr(0, 2);
     if (s.endsWith('PM') && !s.startsWith('12')) {
-        hours = +hours + 12 % 24;
-        s = hours + s.slice(2, 10);
+        hours = +hours + 12;
+        s = hours + s.slice(2, s.length);
     } else if (s.endsWith('AM') && s.startsWith('12')) {
         s = s.replace(/12/, '00');
     }
