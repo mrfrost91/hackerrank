5642f7a74e733715dd14d105f24dad7f35c131ef
mrfrost91 <metallfrost@gmail.com>
2019-06-10 16:25:12 +0300

    Create birthdayChocolate.js

---
+++ birthdayChocolate.js
@@ -0,0 +1,15 @@
+function birthday(s, d, m) {
+    let sum = 0;
+    let result = 0;
+    for (let i = 0; i < s.length - m + 1; i++) {
+        for (let j = i; j < m + i; j++) {
+            sum += s[j];
+        }
+        console.log(sum);
+        if (sum === d) {
+            result++;
+        }
+        sum = 0;
+    }
+    return result
+}
